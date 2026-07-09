1357e688f63af025edb901b3ce03ea5e5a6f1476
you10069 <104882329+you10069@users.noreply.github.com>
2026-07-10 00:58:19 +0800

    Revert "feat: support ipv6 dual stack fallback for hysteria2"
    This reverts commit d63f6ae0bf9453b81e6aeac0b56c3d86843a3422.

--- adapter/outbound/hysteria2.go
+++ adapter/outbound/hysteria2.go
@@ -11,12 +11,12 @@ import (
 	N "github.com/metacubex/mihomo/common/net"
 	"github.com/metacubex/mihomo/common/utils"
 	"github.com/metacubex/mihomo/component/ca"
+	"github.com/metacubex/mihomo/component/proxydialer"
 	C "github.com/metacubex/mihomo/constant"
 	"github.com/metacubex/mihomo/log"
 	tuicCommon "github.com/metacubex/mihomo/transport/tuic/common"
 
 	"github.com/metacubex/quic-go"
-	qtls "github.com/metacubex/sing-quic"
 	"github.com/metacubex/sing-quic/hysteria2"
 	M "github.com/metacubex/sing/common/metadata"
 	"github.com/metacubex/tls"
@@ -118,6 +118,7 @@ func NewHysteria2(option Hysteria2Option) (*Hysteria2, error) {
 		option: &option,
 	}
 	outbound.dialer = option.NewDialer(outbound.DialOptions())
+	singDialer := proxydialer.NewSingDialer(outbound.dialer)
 
 	var salamanderPassword string
 	if len(option.Obfs) > 0 {
@@ -176,6 +177,7 @@ func NewHysteria2(option Hysteria2Option) (*Hysteria2, error) {
 
 	clientOptions := hysteria2.ClientOptions{
 		Context:            context.TODO(),
+		Dialer:             singDialer,
 		Logger:             log.SingLogger,
 		SendBPS:            StringToBps(option.Up),
 		ReceiveBPS:         StringToBps(option.Down),
@@ -186,15 +188,17 @@ func NewHysteria2(option Hysteria2Option) (*Hysteria2, error) {
 		UDPDisabled:        false,
 		CWND:               option.CWND,
 		UdpMTU:             option.UdpMTU,
-		ServerAddress:      M.ParseSocksaddr(addr),
-		PacketListener:     outbound.dialer,
-		QuicDialer: qtls.QuicDialerFunc(func(ctx context.Context, addr string, dialer qtls.PacketDialer, tlsCfg *tls.Config, cfg *quic.Config, early bool) (net.PacketConn, *quic.Conn, error) {
-			err = echConfig.ClientHandle(ctx, tlsCfg)
+		ServerAddress: func(ctx context.Context) (*net.UDPAddr, error) {
+			udpAddr, err := resolveUDPAddr(ctx, "udp", addr, option.IPVersion)
 			if err != nil {
-				return nil, nil, err
+				return nil, err
 			}
-			return tuicCommon.DialQuic(ctx, addr, outbound.DialOptions(), dialer, tlsCfg, cfg, early)
-		}),
+			err = echConfig.ClientHandle(ctx, tlsClientConfig)
+			if err != nil {
+				return nil, err
+			}
+			return udpAddr, nil
+		},
 	}
 
 	var ranges utils.IntRanges[uint16]
